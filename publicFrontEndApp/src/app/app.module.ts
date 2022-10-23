37fda10aa3844648837ce25dc38e4458d7702b25
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-10-23 20:40:49 +0200

    Nabvar, routing, creating feedback design

--- publicFrontEndApp/src/app/app.module.ts
+++ publicFrontEndApp/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FeedbackTableComponent } from './feedback-table/feedback-table.component';
+import { CreateFeedbackComponent } from './create-feedback/create-feedback.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PatientHomeComponent } from './patient-home/patient-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FeedbackTableComponent
+    FeedbackTableComponent,
+    CreateFeedbackComponent,
+    routingComponents,
+    LandingPageComponent,
+    PatientHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
