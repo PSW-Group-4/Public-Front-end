d0ba46fcb2c6719877e11b3d8dbaca14ef336ee1
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-10-23 17:19:08 +0200

    Logo, welcome, login, feedback design

---
+++ publicFrontEndApp/src/app/feedback-table/feedback-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-table',
+  templateUrl: './feedback-table.component.html',
+  styleUrls: ['./feedback-table.component.css']
+})
+export class FeedbackTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
