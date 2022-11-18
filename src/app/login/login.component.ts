eaf1ec9086e7c7c4fada065be8a243b30f4d09dd
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2022-11-18 12:49:36 +0100

    check if link has query params and send request for account activation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,51 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm = new FormGroup({
-      username: new FormControl<string>(''),
-      password: new FormControl<string>('')
+    username: new FormControl<string>(''),
+    password: new FormControl<string>(''),
   });
 
-  get username(){
+  get username() {
     return this.loginForm.controls.username.value;
   }
-  get password(){
+  get password() {
     return this.loginForm.controls.password.value;
   }
 
-  constructor(private readonly loginService:LoginService,
-              private readonly router:Router) { }
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
+    const params = new URLSearchParams(window.location.search);
+    console.log(params);
+    var token = params.get('token');
+    console.log(token);
+    var id = params.get('id');
+    console.log(id);
+
+    if (token !== null && id != null) {
+      this.loginService.activateAccount(token, id).subscribe((res) => {
+        console.log(res);
+      });
+    }
   }
 
-  login = () =>
-  {
+  login = () => {
     let dto = {
-      username:this.username!,
-      password:this.password!
-    }
+      username: this.username!,
+      password: this.password!,
+    };
 
     this.loginService.login(dto).subscribe({
-      next: response =>{
+      next: (response) => {
         localStorage.setItem('jwt', response.jwt);
-        this.router.navigate(['patientHome']);         
+        this.router.navigate(['patientHome']);
       },
       //TODO: handle errors
-      error: err => {
+      error: (err) => {
         alert(err.message);
-      }  
-    }
-    );
-  }
-
+      },
+    });
+  };
 }
