c446de04864283d80de8b9796e68a9a50cffc9a0
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-11-25 00:05:43 +0100

    Update register.component.ts

--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -171,7 +171,7 @@ export class RegisterComponent implements OnInit {
     }
 
     console.log(dto);
-    this.userService.registerUser(dto).subscribe(res => { })
+    this.userService.registerUser(dto).subscribe(res => { this.router.navigate(['loginPage']) })
   }
 
 }
