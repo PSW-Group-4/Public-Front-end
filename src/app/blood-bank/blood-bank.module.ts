05ff8891743f44da465eb8e1a66eb7a4af3c3fee
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2022-12-15 19:05:30 +0100

    Add missing import

--- src/app/blood-bank/blood-bank.module.ts
+++ src/app/blood-bank/blood-bank.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Routes } from '@angular/router';
+import {RouterOutlet, Routes} from '@angular/router';
 import { BloodBankLoginComponent } from './blood-bank-login/blood-bank-login.component';
 import { BloodBankHomeComponent } from './blood-bank-home/blood-bank-home-component';
 
@@ -28,7 +28,8 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    RouterOutlet
   ]
 })
 export class BloodBankModule { }
