92cac6047d502a61a5f79edff280084b85221126
Sanja Petrović <sanjica.petrovic7@gmail.com>
2023-01-17 19:52:28 +0100

    fix: tender

--- src/app/Services/tender.service.ts
+++ src/app/Services/tender.service.ts
@@ -11,14 +11,14 @@ export class TenderService {
   headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) { }
-  
-  approve(): Observable<any> {
-    return this.http.get<any>(this.apiHost + 'api/Tender/approve', {headers: this.headers});
+
+  approve(tenderId: string | null): Observable<any> {
+    return this.http.get<any>(this.apiHost + `api/Tender/${tenderId}/winner/confirm`, {headers: this.headers});
   }
   getActive(): Observable<any[]> {
     return this.http.get<any[]>(this.apiHost + 'api/Tender/active', {headers: this.headers});
   }
-  deny(): Observable<any> {
-    return this.http.get<any>(this.apiHost + 'api/Tender/deny', {headers: this.headers});
+  deny(tenderId: string | null): Observable<any> {
+    return this.http.get<any>(this.apiHost + `api/Tender/${tenderId}/winner/confirm`, {headers: this.headers});
   }
 }
--- src/app/tender-winner-response/tender-winner-response.component.ts
+++ src/app/tender-winner-response/tender-winner-response.component.ts
@@ -1,6 +1,7 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TenderService } from '../Services/tender.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-tender-winner-response',
@@ -9,19 +10,21 @@ import { TenderService } from '../Services/tender.service';
 })
 export class TenderWinnerResponseComponent implements OnInit {
 
-  constructor(private tenderService: TenderService, private scroller: ViewportScroller) { }
-
+  constructor(private tenderService: TenderService, private scroller: ViewportScroller, private route: ActivatedRoute ) { }
+  public tenderId: string | null = "";
   ngOnInit(): void {
+    this.tenderId = this.route.snapshot.paramMap.get('id');
+    console.log(this.route.snapshot.paramMap.get('id'));
   }
 
   goDown() {
     this.scroller.scrollToAnchor("buttons");
   }
   approve(): void {
-    this.tenderService.approve();
+    this.tenderService.approve(this.tenderId);
   }
 
   deny(): void {
-    this.tenderService.deny();
+    this.tenderService.deny(this.tenderId);
   }
 }
